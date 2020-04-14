d9f583921b7221464b5c238d9b42e2519eb53874
Vasilisa <grigvasilisa@gmail.com>
2020-04-14 19:44:23 +0400

    start web

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from data import db_session
-from flask import Flask
+from flask import Flask, render_template, url_for
 from flask_restful import abort, Api
 from users_resource import User, UserResource, UserListResource
 from story_resource import Story, StoryResource, StoryListResource
@@ -7,6 +7,8 @@ from story_resource import Story, StoryResource, StoryListResource
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandex_detective_key'
 api = Api(app)
+
+current_user = None
 
 
 def main():
@@ -16,6 +18,12 @@ def main():
     api.add_resource(StoryListResource, '/api/stories')
     api.add_resource(StoryResource, '/api/stories/<int:story_id>')
     app.run()
+
+
+@app.route("/")
+@app.route('/index')
+def start():
+   return render_template('base.html', current_user=current_user, css_style=url_for('static', filename='css/style.css'))
 
 
 if __name__ == '__main__':
