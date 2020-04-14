612ab7bee0e46357a2a859806a36b44f3d4dd2c5
Vasilisa <grigvasilisa@gmail.com>
2020-04-14 21:33:10 +0400

    register/enter

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 from data import db_session
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 from flask_restful import abort, Api
 from users_resource import User, UserResource, UserListResource
 from story_resource import Story, StoryResource, StoryListResource
+from flask_login import LoginManager, login_required, logout_user, login_user
+
+
+from flask_wtf import FlaskForm
+from wtforms.fields.simple import PasswordField, BooleanField, SubmitField
+from wtforms.fields.core import StringField
+from wtforms.validators import DataRequired
+
+
+class RegisterForm(FlaskForm):
+    nickname = StringField("Никнейм", validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторный пароль', validators=[DataRequired()])
+    submit = SubmitField('Зарегистрироваться')
+
+
+class LoginForm(FlaskForm):
+    nickname = StringField('Никнейм', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandex_detective_key'
 api = Api(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 current_user = None
 
@@ -23,7 +48,64 @@ def main():
 @app.route("/")
 @app.route('/index')
 def start():
-   return render_template('base.html', current_user=current_user, css_style=url_for('static', filename='css/style.css'))
+   return render_template('base.html', current_user=current_user,
+                          css_style=url_for('static', filename='css/style.css'),
+                          background=url_for('static', filename='img/img_start.jpg'))
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    session = db_session.create_session()
+
+    return session.query(User).get(user_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    global current_user
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        session = db_session.create_session()
+        user = session.query(User).filter(User.nickname == form.nickname.data).first()
+        if user and user.check_password(form.password.data):
+            current_user = user
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data == form.password_again.data:
+            session = db_session.create_session()
+            user = User()
+
+            user.nickname = form.nickname.data
+            user.set_password(form.password.data)
+
+            session.merge(user)
+            session.commit()
+            return redirect('/')
+        return render_template('register.html',
+                               message="Пароли не совпадают",
+                               form=form)
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    global current_user
+
+    logout_user()
+    current_user = None
+    return redirect("/")
 
 
 if __name__ == '__main__':
--- users_resource.py
+++ users_resource.py
@@ -1,24 +1,28 @@
-from data import db_session
+import sqlalchemy
+
 from flask import jsonify, abort
 from flask_login import UserMixin
 from flask_restful import Resource
-import sqlalchemy
 from sqlalchemy_serializer import SerializerMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from user_parser import parser
+from data import db_session
 
 
 class User(db_session.SqlAlchemyBase, UserMixin, SerializerMixin):
     __tablename__ = 'users'
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     nickname = sqlalchemy.Column(sqlalchemy.String)
     password = sqlalchemy.Column(sqlalchemy.String)
     submit = sqlalchemy.Column(sqlalchemy.Boolean)
     watched = sqlalchemy.Column(sqlalchemy.String)
 
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
     def check_password(self, password):
-        if password == self.password:
-            return True
-        return False
+        return check_password_hash(self.password, password)
 
 
 def abort_if_user_not_found(user_id):
@@ -54,7 +58,6 @@ class UserListResource(Resource):
         args = parser.parse_args()
         session = db_session.create_session()
         user = User(
-            id=args['id'],
             nickname=args['nickname'],
             password=args['password'],
             submit=args['submit'],
