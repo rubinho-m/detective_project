3ad4f19c5b5ab9556b2379a759ac6e19d0b4181d
Vasilisa <grigvasilisa@gmail.com>
2020-04-28 21:48:30 +0400

    loaded[1:]

--- delete_loaded_img.py
+++ delete_loaded_img.py
@@ -2,7 +2,7 @@ import os
 
 
 def delete_in_directory(folder):
-    for the_file in os.listdir(folder):
+    for the_file in os.listdir(folder)[1:]:
         file_path = os.path.join(folder, the_file)
         try:
             if os.path.isfile(file_path):
